89f8c41a9ce35da0a5cb30d07a8fb782b4c1b5a7
kriti karir <karir.kriti@gmail.com>
2024-09-28 13:35:10 +0530

    exception and logging

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,4 @@
 import sys
-from logger import logging
 
 
 def error_message_detail(error, error_detail:sys):
@@ -20,9 +19,3 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
     
-if __name__=="__main__":
-    try:
-        a=1/0
-    except Exception as err:
-        logging.info("Divide by 0 error.")
-        raise CustomException(err, sys)
